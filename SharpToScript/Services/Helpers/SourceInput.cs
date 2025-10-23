94a79b276c3a942309f7e8dac074d7c4bb536a6b
Ruel Sucgang <ruel.sucgang@gmail.com>
2025-10-24 07:16:14 +0800

    Added TypeScript converter service and centralized source input Implemented TypescriptConverterService to generate TypeScript interfaces from parsed C# models Created SourceInput helper class for storing and managing sample C# input definitions

---
+++ SharpToScript/Services/Helpers/SourceInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpToScript.Services.Helpers
+{
+    internal static class SourceInput
+    {
+        /// <summary>
+        /// The C# class definition string that will be parsed and converted to TypeScript.
+        /// Update this string whenever the system needs a different class structure.
+        /// </summary>
+        public static string CSharpClassDefinition => @"
+            public class PersonDto {
+                public string Name { get; set; }
+                public int Age { get; set; }
+                public string Gender { get; set; }
+                public long? DriverLicenceNumber { get; set; }
+                public List<Address> Addresses { get; set; }
+                public class Address
+                {
+                    public int StreetNumber { get; set; }
+                    public string StreetName { get; set; }
+                    public string Suburb { get; set; }
+                    public int PostCode { get; set; }
+                }
+            }";  
+    }
+}
+
+
